01bea5a4d29de76b675b74785e24e3e69c841ae5
washi <washington.parra@gmail.com>
2021-10-21 07:54:36 -0300

    it works

---
+++ ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuTodo.ViewModels
+{
+    public class CreateTodoViewModel
+    {
+        public string Title { get; set; }
+    }
+}
